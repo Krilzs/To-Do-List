b3031c609898b1d1d4f00926e3d73cdbd3d43e3a
Jeronimo Corigliano <jerofc143@gmail.com>
2024-12-02 19:43:35 -0300

    adding Functions

--- index.js
+++ index.js
@@ -1,137 +1,152 @@
-const user_projects = [];
-const add_project_btn = document.getElementById("add-project-btn");
-const dialog = document.querySelector("#project-modal");
-const dialog_form = document.querySelector("#project-form");
-
-class task {
-  constructor(title, author, description, priority) {
-    this.title = title;
-    this.author = author;
-    this.description = description;
-    this.priority = priority;
-  }
-}
-
 class project {
   constructor(title, description) {
     this.title = title;
     this.description = description;
-    this.project_tasks = [];
-  }
-
-  addTask(title, author, description, priority) {
-    const newTask = new task(title, author, description, priority);
-    this.project_tasks.push(newTask);
-  }
-
-  getTasks() {
-    return this.project_tasks;
+    this.tasks = [];
   }
 }
 
-function displayTasks(project) {
-  const tasks_list = document.querySelector(".tasks-list");
-  tasks_list.innerHTML = "";
-  let tasks_array = [...project.getTasks()];
-  if (tasks_array.length == 0) {
-    const div = document.createElement("div");
-    div.className += "task";
-    div.innerHTML = `
-    <p>No Se encontraron Tareas</p>`;
-    tasks_list.appendChild(div);
-    console.log(tasks_array.lenght);
-  } else {
-    tasks_array.forEach((task) => {
-      const div = document.createElement("div");
-      div.className += "task";
-      div.innerHTML = `
-      <h2>${task.title}</h2>
-      <p>${task.description}</p>
-      <p>${task.author}</p>
-      <p>Nivel de prioridad = ${task.priority}</p>`;
-      tasks_list.appendChild(div);
+class Task {
+  constructor(title, description, author, status) {
+    this.title = title;
+    this.description = description;
+    this.author = author;
+    this.status = status;
+  }
+}
+
+const createBoard = () => {
+  const app = document.getElementById("root");
+  const user_projects = [];
+
+  const board_aside = document.createElement("section");
+  board_aside.classList += "aside";
+  app.appendChild(board_aside);
+
+  const project_list = document.createElement("div");
+  project_list.classList += "project-list";
+  board_aside.appendChild(project_list);
+
+  const board_main = document.createElement("section");
+  board_main.classList += "main-base";
+  board_main.innerHTML = `<h2>Here You can will find your tasks</h2>`;
+  app.appendChild(board_main);
+
+  const task_list = document.createElement("div");
+  task_list.classList += "task-list";
+  board_main.appendChild(task_list);
+
+  const addProject = (title, description) => {
+    const newProject = new project(title, description);
+    user_projects.push(newProject);
+    displayProjects();
+  };
+
+  const addTask = (title, description, author, status, index) => {
+    const newTask = new Task(title, description, author, status);
+    user_projects[index].tasks.push(newTask);
+    displayTasks(index);
+  };
+
+  const deleteTask = (index, task_index) => {
+    user_projects[index].tasks.splice(task_index, 1);
+    displayTasks(index);
+  };
+
+  const displayProjects = () => {
+    project_list.innerHTML = "<h2>My Projects</h2>";
+    if (user_projects.length === 0) {
+      const project_el = document.createElement("div");
+      project_el.innerHTML = "No projects";
+      project_el.id = "no-projects";
+      project_list.appendChild(project_el);
+    }
+    user_projects.forEach((project, index) => {
+      const project_el = document.createElement("div");
+      project_el.innerHTML = project.title;
+      project_el.id = `${index}`;
+      project_el.classList += "project";
+      project_el.addEventListener("click", () => {
+        displayTasks(index);
+      });
+      project_list.appendChild(project_el);
     });
-  }
-}
+  };
 
-function createProject(title, description) {
-  const newProject = new project(title, description);
-  user_projects.push(newProject);
-  console.log("hola");
-  showProjects();
-}
+  const displayTasks = (index) => {
+    board_main.classList = "main";
+    board_main.querySelector(
+      "h2"
+    ).innerHTML = `<h2>${user_projects[index].title}</h2>`;
+    task_list.innerHTML = "";
+    if (user_projects[index].tasks.length === 0) {
+      const task_el = document.createElement("div");
+      task_el.innerHTML = "No tasks";
+      task_el.id = "no-tasks";
+      const add_task_btn = document.createElement("button");
+      add_task_btn.innerHTML = "Add Task";
+      add_task_btn.id = "add-task";
+      add_task_btn.addEventListener("click", () => {
+        const title = prompt("Title");
+        const description = prompt("Description");
+        const author = prompt("Author");
+        const status = prompt("Status");
+        addTask(title, description, author, status, index);
+      });
+      task_list.appendChild(task_el);
+      task_list.appendChild(add_task_btn);
+    } else {
+      user_projects[index].tasks.forEach((task, task_index) => {
+        const delete_task_btn = document.createElement("button");
+        const task_el = document.createElement("div");
+        task_el.innerHTML = ` 
+        <h3>${task.title}</h3>
+        <p>${task.status}</p>
+        `;
+        task_el.classList += "task";
+        task_el.appendChild(delete_task_btn);
+        task_list.appendChild(task_el);
+      });
+      const add_task_btn = document.createElement("button");
+      add_task_btn.innerHTML = "Add Task";
+      add_task_btn.id = "add-task";
+      add_task_btn.addEventListener("click", () => {
+        const title = prompt("Title");
+        const description = prompt("Description");
+        const author = prompt("Author");
+        const status = prompt("Status");
+        addTask(title, description, author, status, index);
+      });
+      task_list.appendChild(add_task_btn);
+    }
+  };
 
-function showProjects() {
-  const list = document.querySelector(".projects-list");
-  list.innerHTML = "";
-  user_projects.forEach((project, i) => {
-    const li = document.createElement("div");
-    li.id = i;
-    li.onclick = function () {
-      displayTasks(user_projects[this.id]);
-    };
-    li.innerHTML = `${project.title} indice ${i}`;
-    list.appendChild(li);
+  const getAside = () => {
+    return board_aside;
+  };
+
+  const getMain = () => {
+    return board_main;
+  };
+
+  return { addProject, getAside, displayProjects, addTask };
+};
+
+const asideManager = () => {
+  const board = createBoard();
+
+  board.displayProjects();
+
+  const add_project_btn = document.createElement("button");
+  add_project_btn.innerHTML = "Add";
+
+  add_project_btn.addEventListener("click", () => {
+    const title = prompt("Title");
+    const description = prompt("Description");
+    board.addProject(title, description);
   });
-}
 
-add_project_btn.addEventListener("click", () => {
-  dialog.showModal();
-});
+  board.getAside().appendChild(add_project_btn);
+};
 
-dialog_form.addEventListener("submit", (e) => {
-  e.preventDefault;
-  const title = document.getElementById("project-title").value;
-  const description = document.getElementById("project-description").value;
-  createProject(title, description);
-  dialog.close();
-});
-
-//Carga de Proyectos
-/*
-createProject("Projecto de Test", "Projecto de prueba");
-createProject("Projecto de Test", "Projecto de prueba");
-createProject("Projecto de Test", "Projecto de prueba");
-createProject("Projecto de Test", "Projecto de prueba");
-createProject("Projecto de Test", "Projecto de prueba");
-createProject("Projecto de Test", "Projecto de prueba");
-
-//Carga de Tareas
-user_projects[0].addTask(
-  "Aprender JavaScript",
-  "Pedro",
-  "Estudiar los fundamentos del lenguaje",
-  2
-);
-user_projects[0].addTask(
-  "Diseñar interfaz",
-  "María",
-  "Crear prototipos en Figma",
-  3
-);
-user_projects[0].addTask(
-  "Configurar servidor",
-  "Luis",
-  "Implementar un servidor en Node.js",
-  4
-);
-user_projects[0].addTask(
-  "Optimizar base de datos",
-  "Ana",
-  "Mejorar consultas en MySQL",
-  5
-);
-user_projects[0].addTask(
-  "Escribir documentación",
-  "Javier",
-  "Redactar guías para usuarios",
-  1
-);
-user_projects[0].addTask(
-  "Realizar pruebas",
-  "Sofía",
-  "Testear funcionalidades críticas",
-  2
-);
-
-*/
+const program = asideManager();
